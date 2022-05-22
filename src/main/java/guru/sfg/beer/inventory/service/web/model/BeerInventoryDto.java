b617b199d687d1ece5c9e54d006528d070ad92f8
Serhii Koptenko <Serhii_Koptenko@epam.com>
2022-05-22 13:17:08 +0300

    add upc

--- src/main/java/guru/sfg/beer/inventory/service/web/model/BeerInventoryDto.java
+++ src/main/java/guru/sfg/beer/inventory/service/web/model/BeerInventoryDto.java
@@ -19,6 +19,7 @@ public class BeerInventoryDto {
     private UUID id;
     private OffsetDateTime createdDate;
     private OffsetDateTime lastModifiedDate;
+    private String upc;
     private UUID beerId;
     private Integer quantityOnHand;
 }
